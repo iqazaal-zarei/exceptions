55448d606fd46bf6a8c8cb2e31fb9c45f0c47dee
iqazzal-zarei <gzarei202@gmail.com>
2023-08-02 21:35:59 +0330

    this is my program

---
+++ exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptions
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            try
+            {
+                Console.WriteLine("Enter a number  :  ");
+                 num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter another number  :  ");
+                 num2 = int.Parse(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("error");
+                Console.ResetColor();
+
+            }
+            finally { Console.WriteLine("finall"); }
+
+            Console.WriteLine("sum is" + (num1 + num2));  
+        }
+    }
+ }
